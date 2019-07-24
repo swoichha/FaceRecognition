25375013af99d798a33ccd35578ad5445734d5e3
swoichha adhikari <swoichhaa@gmail.com>
2019-07-24 19:39:26 +0545

    dimensionality reduction

--- yalefaces/yalefaces/imageops.py
+++ yalefaces/yalefaces/imageops.py
@@ -14,68 +14,65 @@ DATADIR = 'C:/Users/swoichha/Documents/GitHub/FaceRecognition/yalefaces/yaleface
 CATEGORIES = ['subject01','subject02','subject03','subject04','subject05','subject06','subject07','subject08','subject09','subject10','subject11','subject12','subject13','subject14','subject15']
 
 img_count = 0
-imageArray = np.zeros((150,51260),dtype=int,order='C')	#Whether to store multi-dimensional data in row-major (C-style) or column-major (Fortran-style) order in memory.
+normalize_face_vector = np.zeros((150,51260),dtype=np.uint8, order='C')
+# resized_normalize_face_vector = np.zeros((150,51260),dtype=np.uint8, order='C')
+imageArray = np.zeros((150,51260),dtype=np.uint8, order='C')	#Whether to store multi-dimensional data in row-major (C-style) or column-major (Fortran-style) order in memory.
 print(imageArray)
-# img_array = img_array.reshape(150,51260,1)			#150 elements with 51260 rows ans 1 column
-print(imageArray[9].size)								#only from 0-9
-print(imageArray.shape)
+# imageArray = imageArray.reshape(150,51260,1)			#150 elements with 51260 rows ans 1 column
+print('\n',imageArray[9].size)								#only from 0-9
+print('\n',imageArray.shape)
 
-img_count = 0
 dir = ""
+
 for category in CATEGORIES:	
 	path = os.path.join(DATADIR,category) 				#-------path to all directory of images of diff subject
 	for img in os.listdir(path):						#-------iterate through all those images
 		dir = (os.path.join(path,img))
 		img_array = mpimg.imread(dir)
-		img_array.resize(1,233*220)
+		img_array.resize(1,51260)
 		# print(imageArray.shape)
 		# print('\n This is actual array \n',imageArray)
 				
 		imageArray[img_count] = np.array(img_array)
 		# i = i + 1
 		img_count += 1
-		# a = np.copyto(img_array,imageArray,casting='same_kind', where=True)
-		# # print(img_array.shape)
-		# # print('\n This is array after place \n',img_array)
-
-		# a = np.place(img_array,img_array == 0,imageArray)
-		# print(img_array.shape)
-		# print('\n This is array after place \n',img_array)
-
 		
 
-print(img_count)
-# print(img_array[512]) #------0 to 9 only
-print(imageArray[5])		#its size is 51260	
-print(imageArray[9])	
+# print(img_array[512]) 	#------0 to 9 only------#
+print(imageArray[5])		#------its size is 51260--------#	
 
 
 def averageOfArray(x,total):
 	x = np.sum(x, axis=0)
 	x = (x/total)
-
 	return x
 
 avg_faceVector = averageOfArray(imageArray,img_count)
-print(avg_faceVector)
-# # print(avg_faceVector.shape)
-# # print(avg_faceVector[512])
+print(avg_faceVector.shape)
+
 resized_avg_faceVector = np.reshape(avg_faceVector,(233,220))
+
 plt.imshow(resized_avg_faceVector,cmap='gray')
 plt.show()
+
+# print(avg_faceVector[512])		#----from 0 to 51259-----#
 
 
 #------Normalize face vector
 i = 0
 while (i < img_count): 
-	normalize_face_vector = np.array(imageArray[i] - avg_faceVector)
-	normalize_face_vector = np.reshape(normalize_face_vector,(233,220))
+	normalize_face_vector[i] = np.array(imageArray[i] - avg_faceVector)
+	# print('\n normalize_face_vector of image',i,'is', normalize_face_vector[i])
+	resized_normalize_face_vector = np.reshape(normalize_face_vector[i],(233,220))
 	i += 1
-	# plt.imshow(normalize_face_vector,cmap='gray')
+	# plt.imshow(resized_normalize_face_vector,cmap='gray')
 	# plt.show()
 
-#------covariance of matrix	is done to find eigen vector
+print('\n shape  \n',normalize_face_vector.shape)
+
+# #------covariance of matrix	is done to find eigen vector
 covariance_Matrix = np.cov(normalize_face_vector)	
 print('\n covariance matrix \n',covariance_Matrix)
+print('\n shape  \n',covariance_Matrix.shape)		#-----150*150-----#
 
 	
